3375c78ab6e796c38d845a386fa661fea05cc744
Tom C <tomcamp333@gmail.com>
2017-06-02 22:51:36 +0100

    Added skeleton for GameObject class

--- 3D Game Engine/src/com/base/core/GameObject.java
+++ 3D Game Engine/src/com/base/core/GameObject.java
@@ -1,5 +1,33 @@
 package com.base.core;
 
+import java.util.ArrayList;
+
 public class GameObject {
+	private ArrayList<GameObject> children;
 	
+	public GameObject() {
+		children = new ArrayList<GameObject>();
+	}
+	
+	public void addChild(GameObject child) {
+		children.add(child);
+	}
+	
+	public void input() {
+		for(GameObject child : children) {
+			child.input();
+		}
+	}
+	
+	public void update() {
+		for(GameObject child : children) {
+			child.update();
+		}
+	}
+	
+	public void render() {
+		for(GameObject child : children) {
+			child.render();
+		}
+	}
 }
